dd0f73204c7a8c0d1b29f31137c9a75c4733782c
NaamTuhin <tuhinmukk@gmail.com>
2025-02-02 21:20:43 +0530

    Added login components

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -3,16 +3,18 @@ import Link from 'next/link';
 import { ShoppingCart, Menu } from 'lucide-react';
 import Image from 'next/image';
 import { useState } from 'react';
+import Login from "./Login"
 
 export default function Navbar() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
+  const [login,setLogin]= useState(false)
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
   return (
-    <nav className="w-full bg-[#f5f5f0] shadow-md px-4 py-2 flex justify-between items-center fixed top-0 z-10"> 
+    <nav className="w-full bg-[#f5f5f0] shadow-md py-2 flex justify-between items-center fixed top-0 z-10"> 
       <div className="flex items-center">
         <Image src={'/logo.png'} alt='logo' width={80} height={80}  /> 
       </div>
@@ -33,14 +35,39 @@ export default function Navbar() {
             <span className="absolute -top-1 -right-1 bg-red-600 text-white text-xs rounded-full px-1">1</span>
           </div>
         </div>
-        <button className="bg-[#e8e6da] text-[#4a2b2b] px-4 py-1 rounded-sm shadow-lg">Login</button>
+        <button
+        onClick={()=>{
+          setLogin(true)
+          
+
+}} 
+         className="bg-[#e8e6da] text-[#4a2b2b] px-4 py-1 rounded-sm shadow-lg">Login</button>
+
         <div className="md:hidden">
         <button onClick={toggleMobileMenu}>
           <Menu className="text-[#4a2b2b] h-8 w-8" />
         </button>
       </div>
       </div>
-      
+
+      {
+        login && (
+          
+          <div className='absolute w-[100vw] h-[200vw] bg-[#262525ab] flex justify-center items-center text-white'>
+          <Login
+          login={login}
+          setLogin ={setLogin}
+          />
+  
+        </div>
+        )
+      }
+
+
+
+    
+ 
+
 
       {isMobileMenuOpen && (
         <div className="absolute top-full left-0 w-full bg-[#f5f5f0] shadow-md rounded-b-md z-10">
