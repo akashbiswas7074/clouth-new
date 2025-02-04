b69e0a543c12d77f0a976abf8835ddcce28c670f
Md Kaif Sardar <mkaifsard564773@gmail.com>
2025-02-04 20:12:06 +0530

    all code

---
+++ app/form-new/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SectionSelector from "../components/ShirtCustomise/ShirtFeaturesSelect";
+const page = () => {
+  return (
+    <div className="w-full">
+      <SectionSelector />
+    </div>
+  );
+};
+
+export default page;
--- lib/database/models/shirtModel/ShirtModel.ts
+++ lib/database/models/shirtModel/ShirtModel.ts
@@ -1,88 +1,99 @@
-import { Schema, model } from "mongoose";
-import { PocketSchema } from "./PocketModel";
-import { FitSchema } from "./FitModel";
-import { CollarSchema } from "./CollarModel";
-import { CuffSchema } from "./CuffModel";
-import { PlacketSchema } from "./PlacketModel";
-import { BottomSchema } from "./BottomModel";
-import { BackSchema } from "./BackModel";
-import { SleevesSchema } from "./SleeveModel";
-import { MonogramSchema } from "./MonogramModel";
-import { FabricSchema } from "./FabricModel";
-import { ColorSchema } from "./ColorModel";
+import mongoose, { Document, Schema } from "mongoose";
 
-export const ShirtSchema = new Schema(
-  {
-    fit: [
-      {
-        type: FitSchema,
-        required: true,
-      },
-    ],
-    pocket: [
-      {
-        type: PocketSchema,
-        required: true,
-      },
-    ],
-    placket: [
-      {
-        type: PlacketSchema,
-        required: true,
-      },
-    ],
-    bottom: [
-      {
-        type: BottomSchema,
-        required: true,
-      },
-    ],
-    back: [
-      {
-        type: BackSchema,
-        required: true,
-      },
-    ],
-    sleeves: [
-      {
-        type: SleevesSchema,
-        required: true,
-      },
-    ],
-    monogram: [
-      {
-        type: MonogramSchema,
-        required: false, // Optional
-      },
-    ],
-    collar: [
-      {
-        type: CollarSchema,
-        required: true,
-      },
-    ],
-    cuff: [
-      {
-        type: CuffSchema,
-        required: true,
-      },
-    ],
-    fabric: [
-      {
-        type: FabricSchema,
-        required: true,
-      },
-    ],
-    color: [
-      {
-        type: ColorSchema,
-        required: true,
-      },
-    ],
+// Define the interfaces for each object field (e.g., CollarStyle, CuffStyle, etc.)
+interface ProductItem {
+  name: string;
+  image: string;
+  price: string;
+}
+
+interface Shirt {
+  collarStyle?: ProductItem;
+  collarButton?: ProductItem;
+  collarHeight?: ProductItem;
+  cuffStyle?: ProductItem;
+  cuffLinks?: ProductItem;
+  watchCompatible?: boolean;
+  bottom?: ProductItem;
+  back?: ProductItem;
+  pocket?: ProductItem;
+  placket?: ProductItem;
+  sleeves?: ProductItem;
+  fit?: ProductItem;
+  colorId?: mongoose.Schema.Types.ObjectId;
+  fabricId?: mongoose.Schema.Types.ObjectId;
+  monogramId?: mongoose.Schema.Types.ObjectId;
+  measurementId?: mongoose.Schema.Types.ObjectId;
+}
+
+const ShirtSchema = new Schema<Shirt>({
+  collarStyle: {
+    type: Object, // Could store an object for collarStyle details
+    required: false,
   },
-  {
-    timestamps: true,
-  }
-);
+  collarButton: {
+    type: Object, // Could store an object for collarButton details
+    required: false,
+  },
+  collarHeight: {
+    type: Object, // Could store an object for collarHeight details
+    required: false,
+  },
+  cuffStyle: {
+    type: Object, // Could store an object for cuffStyle details
+    required: false,
+  },
+  cuffLinks: {
+    type: Object, // Could store an object for cuffLinks details
+    required: false,
+  },
+  watchCompatible: {
+    type: Boolean, // Store whether the shirt is watch compatible
+    required: false,
+  },
+  bottom: {
+    type: Object, // Could store an object for bottom details
+    required: false,
+  },
+  back: {
+    type: Object, // Could store an object for back details
+    required: false,
+  },
+  pocket: {
+    type: Object, // Could store an object for pocket details
+    required: false,
+  },
+  placket: {
+    type: Object, // Could store an object for placket details
+    required: false,
+  },
+  sleeves: {
+    type: Object, // Could store an object for sleeves details
+    required: false,
+  },
+  fit: {
+    type: Object, // Could store an object for fit details
+    required: false,
+  },
+  colorId: {
+    type: mongoose.Schema.Types.ObjectId, // Reference to Color model
+    required: false,
+  },
+  fabricId: {
+    type: mongoose.Schema.Types.ObjectId, // Reference to Fabric model
+    required: false,
+  },
+  monogramId: {
+    type: mongoose.Schema.Types.ObjectId, // Reference to Monogram model
+    required: false,
+  },
+  measurementId: {
+    type: mongoose.Schema.Types.ObjectId, // Reference to Measurement model
+    required: false,
+  },
+});
 
-export default model("Shirt", ShirtSchema);
+// Create the model using the schema
+const ShirtModel = mongoose.models.Shirt || mongoose.model<Shirt>("Shirt", ShirtSchema);
+
+export default ShirtModel;
